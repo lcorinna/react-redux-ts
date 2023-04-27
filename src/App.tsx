a3bddd9754c715d945309b0b15bb7b52ec74d81d
Lcorinna <Gaida95@yandex.ru>
2023-04-27 11:47:58 +0300

    v1.0

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import UserList from "./components/userList";
+import TodoList from "./components/todoList";
+
+const App = () => {
+  return (
+    <div>
+      <UserList/>
+      <hr/>
+      <TodoList/>
+    </div>
+  );
+};
+
+export default App;
